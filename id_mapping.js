4170d154d6388227ad06808b7b0f75cb95e1438c
Garciaj66 <126178731+Garciaj66@users.noreply.github.com>
2026-01-29 10:42:51 -0600

    Update id_mapping.js

--- id_mapping.js
+++ id_mapping.js
@@ -1,8 +1,9 @@
 const redirects = {
-    "ID1": "https://facebook.com",
-    "ID2": "https://google.com",
-    "ID3": "https://github.com",
-    "powerbi": "https://app.powerbi.com",
-    "mi-linkedin": "https://linkedin.com/in/tu-usuario"
+    "ID1": "http://vantive.sharepoint.com",
+    "ID2": "https://service.baxter.com/tsportal/",
+    "ID3": "https://e1lapd.aws.baxter.com/jde/E1Menu.maf",
+    "ID4": "https://coreservices.vantive.corp/Ernie/Accept.aspx",        
+    "ID5": "https://baxcare.my.salesforce.com",
+    "ID6": "https://apps.powerapps.com/play/e/default-4db037bb-7f1e-40a0-add2-df4a6079d204/a/b8cb67da-3d15-4525-87f6-5ad4fcfb7cce?tenantId=4db037bb-7f1e-40a0-add2-df4a6079d204&hint=c7a6ad14-263d-42be-936d-0a25c2e2403c&sourcetime=1769704955896"
     // Add your new links here following the format: "ID": "DESTINATION",
 };

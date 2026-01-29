7c864b5817ebe40a12fc54249da2d895351d30a1
Garciaj66 <126178731+Garciaj66@users.noreply.github.com>
2026-01-29 09:48:50 -0600

    Create ID_Mapping.js

---
+++ ID_Mapping.js
@@ -0,0 +1,8 @@
+const redirects = {
+    "ID1": "https://facebook.com",
+    "ID2": "https://google.com",
+    "ID3": "https://github.com",
+    "powerbi": "https://app.powerbi.com",
+    "mi-linkedin": "https://linkedin.com/in/tu-usuario"
+    // Add your new links here following the format: "ID": "DESTINATION",
+};
